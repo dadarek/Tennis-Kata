999b93d4d2fc8e66be2418e8d36ec5298bf06df6
Dariusz Pasciak <dariusz@8thlight.com>
2012-04-04 14:34:22 -0500

    First test.

--- lib/tennis.rb
+++ lib/tennis.rb
@@ -1,6 +1,10 @@
 
 class Tennis
-  def say_hi
+  def p1_score 
+    0
+  end
 
+  def p2_score
+    0
   end
 end
--- spec/tennis_spec.rb
+++ spec/tennis_spec.rb
@@ -1,8 +1,9 @@
 require 'tennis'
 
 describe Tennis do
-  it "says hi" do
+  it "starts with 0 points" do
     x = Tennis.new
-    x.say_hi
+    x.p1_score.should == 0
+    x.p2_score.should == 0
   end
 end
