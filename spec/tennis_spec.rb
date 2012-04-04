772346968e7d2093283b17958993392b94f4269c
Dariusz Pasciak <dariusz@8thlight.com>
2012-04-04 15:00:27 -0500

    First test.

--- spec/tennis_spec.rb
+++ spec/tennis_spec.rb
@@ -5,4 +5,10 @@ describe Tennis do
     @game = Tennis.new
     @game.score.should == [0, 0]
   end
+
+  it "remembers score" do
+    @game = Tennis.new
+    @game.point :p1
+    @game.score.should == [15, 0]
+  end
 end
