79ebc91818e63320aa13beb03206085cdd0cc2de
Dariusz Pasciak <dariusz@8thlight.com>
2012-04-04 16:15:42 -0500

    Starting over.

--- lib/tennis.rb
+++ lib/tennis.rb
@@ -1,36 +1,11 @@
 
 class Tennis
-  def initialize
-    @index = { :p1 => 0, :p2 => 0 }
-    @scores = [0, 15, 30, 40]
-  end
 
   def score
-    [score_of(:p1), score_of(:p2)]
-  end
-
-  def point player
-    @index[player] += 1
+    [0, 0]
   end
 
   def winner
-    return :p1 if @index[:p1] >= 4 and p1_is_ahead_2
-    return :p2 if @index[:p2] >= 4 and p2_is_ahead_2
-  end
-
-  private
-
-  def p1_is_ahead_2
-    @index[:p1] - @index[:p2] > 1
-  end
-
-  def p2_is_ahead_2
-    @index[:p2] - @index[:p1] > 1
-  end
-
-  def score_of player
-    @scores[@index[player]]
   end
 
 end
-
--- spec/tennis_spec.rb
+++ spec/tennis_spec.rb
@@ -1,80 +1,11 @@
 require 'tennis'
 
 describe Tennis do
-  before :each do
-    @game = Tennis.new
+
+  it "starts with 0 score and no winner" do
+    game = Tennis.new
+    game.score.should == [0, 0]
+    game.winner.should == nil
   end
 
-  it "starts with 0 points" do
-    assert_score [0, 0]
-  end
-
-  it "keeps track of first point" do
-    @game.point :p1
-    assert_score [15, 0]
-  end
-
-  it "keeps track of second point" do
-    2.times { @game.point :p1 }
-    assert_score [30, 0]
-  end
-
-  it "keeps track of third point" do
-    3.times { @game.point :p1 }
-    assert_score [40, 0]
-  end
-
-  it "keeps track of player 2 points" do
-    @game.point :p2
-    assert_score [0, 15]
-  end
-
-  it "keeps track of both player points" do
-    point_n_times 1, 2
-    assert_score [15, 30]
-  end
-
-  it "declares p1 as winner after 4 points" do
-    4.times { @game.point :p1 }
-    @game.winner.should == :p1
-  end
-
-  it "declares p2 as winner after 4 points" do
-    4.times { @game.point :p2 }
-    @game.winner.should == :p2
-  end
-
-  it "declares no winner if p1 has 3 points" do
-    point_n_times 3, 4
-    @game.winner.should == nil
-  end
-
-  it "declares no winner if p2 has 3 points" do
-    point_n_times 4, 3
-    @game.winner.should == nil
-  end
-
-  it "declares p1 winner if p2 has 3 points but p1 has 5" do
-    point_n_times 5, 3
-    @game.winner.should == :p1
-  end
-
-  it "declares p1 winner if p2 has 4 points but p1 has 6" do
-    point_n_times 6, 4
-    @game.winner.should == :p1
-  end
-
-  it "declares p2 winner if p1 has 4 points but p2 has 6" do
-    point_n_times 4, 6
-    @game.winner.should == :p2
-  end
-
-  def point_n_times p1, p2
-    p1.times { @game.point :p1 }
-    p2.times { @game.point :p2 }
-  end
-
-  def assert_score expected
-    @game.score.should == expected
-  end
 end
