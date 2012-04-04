009fd57cc46c1b943cde80c4ebe71b3ebb122b60
Dariusz Pasciak <dariusz@8thlight.com>
2012-04-04 15:24:14 -0500

    Refactoring.

--- spec/tennis_spec.rb
+++ spec/tennis_spec.rb
@@ -10,22 +10,18 @@ describe Tennis do
   end
 
   it "keeps track of first point" do
-    score_n_times 1
+    @game.point :p1
     assert_score 15
   end
 
   it "keeps track of second point" do
-    score_n_times 2
+    2.times { @game.point :p1 }
     assert_score 30
   end
 
   it "keeps track of third point" do
-    score_n_times 3
+    3.times { @game.point :p1 }
     assert_score 40
-  end
-
-  def score_n_times n
-    n.times { @game.point :p1 }
   end
 
   def assert_score expected
