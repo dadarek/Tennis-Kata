8ebf4812d875747aa73ced3ad3c24f2daead48a8
Dariusz Pasciak <dariusz@8thlight.com>
2012-04-04 16:26:24 -0500

    Refactoring.

--- spec/tennis_spec.rb
+++ spec/tennis_spec.rb
@@ -11,17 +11,18 @@ describe Tennis do
   end
 
   it "treats first point as 15" do
-    @game.point :p1
-    @game.point :p2
+    points 1, 1
     @game.score.should == [15, 15]
   end
 
   it "treats second point as 30" do
-    @game.point :p1
-    @game.point :p2
-    @game.point :p1
-    @game.point :p2
+    points 2, 2
     @game.score.should == [30, 30]
+  end
+
+  def points p1, p2
+    p1.times { @game.point :p1 }
+    p2.times { @game.point :p2 }
   end
 
 end
