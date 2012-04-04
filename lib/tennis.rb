0c8be946b2f85d55260bd1164b3d5597cdc64a83
Dariusz Pasciak <dariusz@8thlight.com>
2012-04-04 14:58:25 -0500

    Starting fresh.

--- lib/tennis.rb
+++ lib/tennis.rb
@@ -1,14 +1,6 @@
 
 class Tennis
-  def initialize
-    @scores = { :p1 => 0, :p2 => 0 }
-  end
-
   def score(player)
-    @scores[player]
   end
 
-  def point(player)
-    @scores[player] = 15
-  end
 end
--- spec/tennis_spec.rb
+++ spec/tennis_spec.rb
@@ -1,28 +1,6 @@
 require 'tennis'
 
 describe Tennis do
-  before(:each) do
-    @game = Tennis.new
-  end
-
   it "starts with 0 points" do
-    @game.score(:p1).should == 0
-    @game.score(:p2).should == 0
   end
-
-  it "adds up points" do
-    @game.point :p1
-    @game.score(:p1).should == 15
-  end
-
-  it "keeps scores separate" do
-    @game.point :p1
-    @game.score(:p2).should == 0
-  end
-
-  it "adds up p2 points" do
-    @game.point :p2
-    @game.score(:p2).should == 15
-  end
-
 end
