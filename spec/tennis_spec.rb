fa6146e0e79fb5ea9d8de4bdf8f96e9cdb9034de
Dariusz Pasciak <dariusz@8thlight.com>
2012-04-04 15:05:41 -0500

    Refactoring.

--- spec/tennis_spec.rb
+++ spec/tennis_spec.rb
@@ -1,19 +1,20 @@
 require 'tennis'
 
 describe Tennis do
-  it "starts with 0 points" do
+  before :each do
     @game = Tennis.new
+  end
+
+  it "starts with 0 points" do
     @game.score.should == [0, 0]
   end
 
   it "remembers p1 score" do
-    @game = Tennis.new
     @game.point :p1
     @game.score.should == [15, 0]
   end
 
   it "remembers p2 score" do
-    @game = Tennis.new
     @game.point :p2
     @game.score.should == [0, 15]
   end
