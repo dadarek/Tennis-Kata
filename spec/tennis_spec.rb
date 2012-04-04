1e4a4affc823b74cb678657f9ea8f66053e314ae
Dariusz Pasciak <dariusz@8thlight.com>
2012-04-04 16:34:44 -0500

    Refactoring.

--- spec/tennis_spec.rb
+++ spec/tennis_spec.rb
@@ -7,32 +7,30 @@ describe Tennis do
   end
 
   it "starts with 0 score" do
-    @game.score.should == [0, 0]
+    assert_points_create_score 0, 0, [0, 0]
   end
 
   it "treats first point as 15" do
-    points 1, 1
-    @game.score.should == [15, 15]
+    assert_points_create_score 1, 1, [15, 15]
   end
 
   it "treats second point as 30" do
-    points 2, 2
-    @game.score.should == [30, 30]
+    assert_points_create_score 2, 2, [30, 30]
   end
 
   it "treats third point as 40" do
-    points 3, 3
-    @game.score.should == [40, 40]
+    assert_points_create_score 3, 3, [40, 40]
   end
 
   it "treats fourth point as 45" do
-    points 4, 3
-    @game.score.should == [45, 40]
+    assert_points_create_score 4, 3, [45, 40]
   end
 
-  def points p1, p2
-    p1.times { @game.point :p1 }
-    p2.times { @game.point :p2 }
+  def assert_points_create_score p1, p2, expected_score
+    game = Tennis.new
+    p1.times { game.point :p1 }
+    p2.times { game.point :p2 }
+    game.score.should == expected_score
   end
 
 end
