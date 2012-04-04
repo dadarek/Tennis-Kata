8f222691f4aecd557bf0596704f047b5fa3e666d
Dariusz Pasciak <dariusz@8thlight.com>
2012-04-04 16:02:34 -0500

    Refactoring.

--- lib/tennis.rb
+++ lib/tennis.rb
@@ -1,8 +1,7 @@
 
 class Tennis
   def initialize
-    @p1ScoreIndex = 0
-    @p2ScoreIndex = 0
+    @index = { :p1 => 0, :p2 => 0 }
     @scores = [0, 15, 30, 40]
   end
 
@@ -11,31 +10,30 @@ class Tennis
   end
 
   def point player
-    @p1ScoreIndex += 1 if player == :p1
-    @p2ScoreIndex += 1 if player == :p2
+    @index[player] += 1
   end
 
   def winner
-    return :p1 if @p1ScoreIndex >= 4 and p1_is_ahead_2
-    return :p2 if @p2ScoreIndex >= 4 and p2_is_ahead_2
+    return :p1 if @index[:p1] >= 4 and p1_is_ahead_2
+    return :p2 if @index[:p2] >= 4 and p2_is_ahead_2
   end
 
   private
 
   def p1_is_ahead_2
-    @p1ScoreIndex - @p2ScoreIndex > 1
+    @index[:p1] - @index[:p2] > 1
   end
 
   def p2_is_ahead_2
-    @p2ScoreIndex - @p1ScoreIndex > 1
+    @index[:p2] - @index[:p1] > 1
   end
 
   def p1_score
-    @scores[@p1ScoreIndex]
+    @scores[@index[:p1]]
   end
 
   def p2_score
-    @scores[@p2ScoreIndex]
+    @scores[@index[:p2]]
   end
 
 end
