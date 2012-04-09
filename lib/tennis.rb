1cb19126525db4be227b809c89eeeb3ef908819e
Dariusz Pasciak <dariusz@8thlight.com>
2012-04-09 17:29:31 -0500

    Starting over.

--- lib/tennis.rb
+++ lib/tennis.rb
@@ -1,47 +1,5 @@
 
 class Tennis
 
-  def initialize
-    @points = { :p1 => 0, :p2 => 0 }
-  end
-
-  def score
-    [score_of(:p1), score_of(:p2)]
-  end
-
-  def point p
-    @points[p] += 1
-  end
-
-  private 
-
-  def has_advantage p
-    player_points = @points[p]
-    opponent_points = @points[ opponent_of p ]
-
-    player_points > opponent_points and player_points > 3
-  end
-
-  def won p
-    player_points = @points[p]
-    opponent_points = @points[ opponent_of p ]
-
-    player_points > opponent_points+1 and player_points > 3
-  end
-
-  def opponent_of p
-    return :p1 if p == :p2
-    :p2
-  end
-
-  def score_of p
-    points = @points[p]
-
-    result = 40 
-    result = points * 15 if points < 3
-    result = 45 if has_advantage p
-    result = 50 if won p
-    result 
-  end
 
 end
--- spec/tennis_spec.rb
+++ spec/tennis_spec.rb
@@ -2,28 +2,5 @@ require 'tennis'
 
 describe Tennis do
 
-  it "keeps proper score" do
-    assert_points_create_score 0, 0, [0, 0]
-    assert_points_create_score 1, 0, [15, 0]
-    assert_points_create_score 2, 0, [30, 0]
-    assert_points_create_score 3, 0, [40, 0]
-    assert_points_create_score 1, 1, [15, 15]
-    assert_points_create_score 2, 2, [30, 30]
-    assert_points_create_score 3, 3, [40, 40]
-    assert_points_create_score 4, 3, [45, 40]
-    assert_points_create_score 4, 4, [40, 40]
-    assert_points_create_score 5, 4, [45, 40]
-    assert_points_create_score 5, 5, [40, 40]
-    assert_points_create_score 5, 6, [40, 45]
-    assert_points_create_score 5, 7, [40, 50]
-    assert_points_create_score 6, 7, [40, 45]
-  end
-
-  def assert_points_create_score p1, p2, expected_score
-    game = Tennis.new
-    p1.times { game.point :p1 }
-    p2.times { game.point :p2 }
-    game.score.should == expected_score
-  end
 
 end
