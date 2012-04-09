160263f7abbb90cd4d159b2b4d1d7d401d2c8a72
Dariusz Pasciak <dariusz@8thlight.com>
2012-04-09 17:52:38 -0500

    Refactoring.

--- lib/tennis.rb
+++ lib/tennis.rb
@@ -9,14 +9,21 @@ class Tennis
   end
 
   def point player
-    points_to_add = 15
-    points_to_add = 10 if @score[player] > 15
-    points_to_add = 5 if score == [40, 40] 
-    points_to_add = 5 if score == [45, 40] or score == [40, 45]
+    @score[player] += next_point_value(player)
+    set_deuce if score == [45, 45]
+  end
 
-    @score[player] += points_to_add
+  def set_deuce
+    @score = { p1: 40, p2: 40 }
+  end
 
-    @score = { p1: 40, p2: 40 } if score == [45, 45]
+  def next_point_value player
+    result = 15
+    result = 10 if @score[player] > 15
+    result = 5 if score == [40, 40] 
+    result = 5 if score == [45, 40] or score == [40, 45]
+
+    result
   end
 
 end
