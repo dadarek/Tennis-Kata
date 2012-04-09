1988f5a694038c196f8bfcf21d262f72d66a049a
Dariusz Pasciak <dariusz@8thlight.com>
2012-04-09 17:42:08 -0500

    Refactoring.

--- spec/tennis_spec.rb
+++ spec/tennis_spec.rb
@@ -11,34 +11,32 @@ describe Tennis do
   end
 
   it "adds 15 to p1's first point" do
-    @game.point :p1
+    score_points :p1, 1
     @game.score.should == [15, 0]
   end
 
   it "adds 15 to p2's first point" do
-    @game.point :p2
+    score_points :p2, 1
     @game.score.should == [0, 15]
   end
 
   it "adds 15 to p1's second point" do
-    @game.point :p1
-    @game.point :p1
+    score_points :p1, 2
     @game.score.should == [30, 0]
   end
 
   it "adds 10 to third point" do
-    @game.point :p1
-    @game.point :p1
-    @game.point :p1
+    score_points :p1, 3
     @game.score.should == [40, 0]
   end
 
   it "adds 10 to fourth point" do
-    @game.point :p1
-    @game.point :p1
-    @game.point :p1
-    @game.point :p1
+    score_points :p1, 4
     @game.score.should == [50, 0]
+  end
+
+  def score_points player, points
+    points.times { @game.point player }
   end
 
 end
