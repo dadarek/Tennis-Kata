94c42ab93957645ef22bddcc0290627dc78b9cc4
Dariusz Pasciak <dariusz@8thlight.com>
2012-04-09 17:30:09 -0500

    First test.

--- lib/tennis.rb
+++ lib/tennis.rb
@@ -1,5 +1,8 @@
 
 class Tennis
 
+  def score
+    [0, 0]
+  end
 
 end
--- spec/tennis_spec.rb
+++ spec/tennis_spec.rb
@@ -2,5 +2,9 @@ require 'tennis'
 
 describe Tennis do
 
+  it "starts with no score" do
+    game = Tennis.new
+    game.score.should == [0, 0]
+  end
 
 end
