29638ef36a45ef7a5abc11aba9bd35caa14b60dd
Dariusz Pasciak <dariusz@8thlight.com>
2012-04-24 07:26:48 -0500

    Tuesday mornin...

--- lib/tennis_scorer.rb
+++ lib/tennis_scorer.rb
@@ -9,34 +9,36 @@ class TennisScorer
     @score.values
   end
 
+
   def ball_for player
     @score[player] += next_point_value player
-    set_deuce if lost_advantage?
+
+    set_deuce if lost_advantage? 
   end
 
-  private
+  private 
 
-  def deuce?
-    score == [40, 40]
+  def next_point_value player
+    points_to_add = 15
+    points_to_add = 10 if @score[player] >= 30
+    points_to_add = 5 if deuce? or advantage? 
+    points_to_add
   end
 
   def advantage?
     score.include? 45
   end
 
-  def lost_advantage?
-    score == [45, 45]
-  end
-
   def set_deuce
     @score = { p1: 40, p2: 40 }
   end
+  
+  def deuce?
+    score == [40, 40]
+  end
 
-  def next_point_value player
-    points_to_add = 15
-    points_to_add = 10 if @score[player] >= 30
-    points_to_add = 5 if deuce? or advantage?
-    points_to_add
+  def lost_advantage?
+    score == [45, 45]
   end
 
 end
--- spec/tennis_scorer_spec.rb
+++ spec/tennis_scorer_spec.rb
@@ -6,26 +6,26 @@ describe TennisScorer do
     @scorer = TennisScorer.new
   end
 
-  it "starts at 0-0" do
+  it "starts off at 0-0" do
     @scorer.score.should == [0, 0]
   end
 
-  it "treats first ball as 15" do
+  it "counts first balls as 15" do
     balls_for :p1, 1
     @scorer.score.should == [15, 0]
   end
 
-  it "treats the second ball as 30" do
+  it "counts the second ball as 30" do
     balls_for :p1, 2
     @scorer.score.should == [30, 0]
   end
 
-  it "treats the third ball as 40" do
+  it "counts the third ball as 40" do
     balls_for :p1, 3
     @scorer.score.should == [40, 0]
   end
 
-  it "treats the fourth ball as 50" do
+  it "counts the fourth ball as 50" do
     balls_for :p1, 4
     @scorer.score.should == [50, 0]
   end
@@ -35,22 +35,23 @@ describe TennisScorer do
     @scorer.score.should == [0, 50]
   end
 
-  it "treats advantage as 45" do
+  it "treats advantage ball as 45" do
     create_advantage :p1
     @scorer.score.should == [45, 40]
   end
 
-  it "treats game-ball as 50" do
+  it "treats game-point on advantage as 50" do
     create_advantage :p1
     balls_for :p1, 1
     @scorer.score.should == [50, 40]
   end
 
-  it "resets to 40-40 on lost advantage" do
-    create_advantage :p1
-    balls_for :p2, 1
+  it "resets to 40-40 after lost advnatage" do
+    create_advantage :p2
+    balls_for :p1, 1
     @scorer.score.should == [40, 40]
   end
+
 
   def balls_for player, how_many
     how_many.times{ @scorer.ball_for player }
